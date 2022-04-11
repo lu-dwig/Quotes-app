f4159fca90f5d03d7dcaa3b98509776a9bd29770
ludwig <murimiludwig@gmail.com>
2022-04-11 16:38:37 +0300

    this will copy files to the gh-pages

--- src/app/highlight-quote.directives.spec.ts
+++ src/app/highlight-quote.directives.spec.ts
@@ -2,7 +2,7 @@ import { HighlightQuoteDirective } from './highlight-quote.directives';
 
 describe('HighlightQouteDirective', () => {
   it('should create an instance', () => {
-    const directive = new HighlightQuoteDirective();
+    const directive = new HighlightQuoteDirective ();
     expect(directive).toBeTruthy();
   });
 });
